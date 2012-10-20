55113f8ead6e191fe440e74900c8412394877929
Puneet Singh <puneet.mir@gmail.com>
2012-10-21 00:39:36 +0530

    part 1 completed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+ def self.rating_list
+  rating = Movie.select("distinct(rating)").order("rating")
+  return rating.collect{|x| x.rating}
+ end
+
+ def self.sort_rate(ratings, sort_by)
+  keys = []
+  keys = ratings.keys if !ratings.nil?
+  data = Movie.where(:rating=>keys)
+  return data.order(sort_by)
+ end
 end
